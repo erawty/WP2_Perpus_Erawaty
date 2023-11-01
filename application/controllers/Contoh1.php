310e7fde79eddbbefbc0bbd68d4a7634022ef14c
erawty <eraw38750@gmail.com>
2023-11-01 21:54:12 +0700

    TUGAS PERTEMUAN 6

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Erawaty saya tinggal di daerah Pontianak olaraga yang saya sukai adalah Renang";
+        
+    }
+} ?>
